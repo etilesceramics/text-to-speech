54523dc63054e0de2189981f088885bfa7ffeeb6
Manas Behera <144924688+etilesceramics@users.noreply.github.com>
2023-12-25 06:49:20 +0300

    Add files via upload

---
+++ texttospeech.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from tkinter import *
+import pyttsx3
+
+engine=pyttsx3.init()
+
+def speaknow():
+    engine.say(textv.get())
+    engine.runAndWait()
+    engine.stop()
+
+root=Tk()
+
+textv=StringVar()
+
+
+obj=LabelFrame(root,text="Text to speech",fg="black",font=20,bg="blue",bd=1)
+
+obj.pack(fill="both",expand="yes",padx=10,pady=10)
+
+lbl=Label(obj,text="Type",bg="blue",font=30)
+lbl.pack(side=tk.LEFT,padx=5)
+
+text=Entry(obj,textvariable=textv,font=30,width=25,bd=5)
+text.pack(side=tk.LEFT,padx=10)
+
+btn=Button(obj,text="Speak",font=20,bg="black",fg="white",command=speaknow)
+btn.pack(side=tk.LEFT,padx=10)
+
+
+root.title("Text to speech")
+root.geometry("700x500")
+root.resizable(False,False)
+root.mainloop()
